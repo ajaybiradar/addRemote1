217ecedc4664e6cb2b0b864c57bf1c9fa9d9c9b7
Ajay Biradar <ajaygbiradar@gmail.com>
2023-04-14 00:52:42 +0530

    printing ex values

---
+++ demo.js
@@ -0,0 +1,17 @@
+var ex = [{ "values": "ITIL User - 2" }, 
+{ "values": "Beth Anglin - 1" },
+ { "values": "Fred Luddy - 3" }, 
+ { "values": "Don Goodliffe - 1" }, 
+ { "values": "Fred Luddy - 3" }, 
+ { "values": "Abraham Lincoln - 1" }, 
+ { "values": "David Dan - 2" }, 
+ { "values": "Bow Ruggeri - 4" }, 
+ { "values": "Abel Tuter (architect) - 1" }, 
+ { "values": "Don Goodliffe - 1" }, 
+ { "values": "Howard Johnson - 1" }, 
+ { "values": "Beth Anglin - 1" }, 
+ { "values": "David Loo - 2" }, 
+ { "values": "Bow Ruggeri - 4" }]
+
+ var obj = JSON.parse(ex);
+ console.log(obj);
